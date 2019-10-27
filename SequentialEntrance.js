da0e98e793fffbaf118d97bc95357520ed2c429f
Teo Man <teo@rafta.co>
2019-10-27 16:40:18 +0300

    add limit

--- SequentialEntrance.js
+++ SequentialEntrance.js
@@ -1,26 +1,32 @@
 export default {
-  functional: true,
-  render(createElement, { props, data, children })
-  {
-    let delay = props.delay || 250;
-    let tag = props.tag || "span";
-    let animation = props.animation || "entranceFromRight";
+    functional: true,
+    render(createElement, {
+        props,
+        data,
+        children
+    }) {
+        let delay = props.delay || 250;
+        let tag = props.tag || "span";
+        let animation = props.animation || "entranceFromRight";
+        let limit = props.limit || 4;
 
-    if(props.fromTop != undefined) animation = 'entranceFromTop'
-    if(props.fromRight != undefined) animation = 'entranceFromRight'
-    if(props.fromBottom != undefined) animation = 'entranceFromBottom'
-    if(props.fromLeft != undefined) animation = 'entranceFromLeft'
+        if (props.fromTop != undefined) animation = 'entranceFromTop'
+        if (props.fromRight != undefined) animation = 'entranceFromRight'
+        if (props.fromBottom != undefined) animation = 'entranceFromBottom'
+        if (props.fromLeft != undefined) animation = 'entranceFromLeft'
 
-    if (children) {
-      children.forEach((child, index) => {
-        child.data.staticStyle = {
-          opacity: 0,
-          animationFillMode: "forwards",
-          animationDelay: index * delay + "ms"
-        };
-        child.data.staticClass += " " + animation;
-      });
+        if (children) {
+            children.forEach((child, index) => {
+				if(index < limit) {
+					child.data.staticStyle = {
+	                    opacity: 0,
+	                    animationFillMode: "forwards",
+	                    animationDelay: index * delay + "ms"
+	                };
+	                child.data.staticClass += " " + animation;
+				}
+            });
+        }
+        return createElement(tag, children);
     }
-    return createElement(tag,children);
-  }
 };
